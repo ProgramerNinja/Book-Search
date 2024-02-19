f263191f4c93b5db7a27649857b6e7c5a6e22db9
ProgramerNinja <nohamcmjt@gmail.com>
2024-02-18 23:20:08 -0700

    reverting useless changes

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 require('dotenv').config();
 const { authMiddleware } = require('./utils/auth');
-const cors = require('cors');
+
 const { typeDefs, resolvers } = require('./schemas');
 const connectDB = require('./config/connection');
 
@@ -20,10 +20,8 @@ const startApolloServer = async () => {
   await server.start();
   await connectDB(); 
 
-  app.use(express.urlencoded({ extended: true }));
+  app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
-
-  app.use(cors());
 
   app.use('/graphql', expressMiddleware(server, {
     context: authMiddleware
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,18 +1,14 @@
-const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 
-const secret = process.env.SECRET || 'mysecret';
+// set token secret and expiration date
+const secret = process.env.SECRET;
 const expiration = '2h';
 
 module.exports = {
-  AuthenticationError: new GraphQLError('Could not authenticate user.', {
-    extensions: {
-      code: 'UNAUTHENTICATED',
-    },
-  }),
-  authMiddleware: function ({ req }) {
-    // allows token to be sent via req.body, req.query, or headers
-    let token = req.body.token || req.query.token || req.headers.authorization;
+  // function for our authenticated routes
+  authMiddleware: function ({req}) {
+    // allows token to be sent via  req.query or headers
+    let token = req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -20,20 +16,23 @@ module.exports = {
     }
 
     if (!token) {
-      return req;
+      return req
     }
 
+    // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
+      return req;
     }
 
-    return req;
+    // send to next endpoint
+   return req
   },
-  signToken: function ({ firstName, email, _id }) {
-    const payload = { firstName, email, _id };
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
