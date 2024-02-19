d017bb61664f981cadf1e479cda23d3aa1e1db48
ProgramerNinja <42654855+ProgramerNinja@users.noreply.github.com>
2024-02-18 18:27:23 -0700

    Update vite.config.js
    Just trying stuff

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 10000,
     open: true,
     proxy: {
       '/graphql': {
-        target: 'http://localhost:3001/graphql',
+        target: 'http://localhost:10000/graphql',
         secure: false,
         changeOrigin: true
       }
